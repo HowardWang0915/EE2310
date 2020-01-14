276b76c3cf8902b780e99a7222dad12d78b79a4e
HowardWang0915 <allmembers2@gmail.com>
2020-01-14 20:32:55 +0800

    Final

--- lab05/lab05.c
+++ lab05/lab05.c
@@ -7,65 +7,49 @@ Date: 2018.10.22
 #define N 7						// macro N to determine the array
 
 int main(void) {
-	int i, j, k, m, a[N], temp, stay, counter = 1;
-	/* i as a counter for a[N] to store numbers, it either 
-	counts up or counts down. j as a[j] is the one two 
-	swap with a[k], k as a[k], a[N] is the array, temp
-	to help swap two arrays, stay is to determine when to 
-	jump out a loop, counter counts how many permutations
-	are there. */
-	printf("permutation #%d:", counter);	// first line
-	for (i = 0; i < N; i++) {		// find out the first permutaion  
-		a[i] = i + 1;
-		printf("%2d", a[i]);   // prints out number from small to big
-	}
-	printf("\n");
-	/* if stay == 3, there are no more a[j] < a[j + 1], then
-	all the permutations found */
-	while (stay != 3) {		
-		for (i = N - 2, stay = 0, j = -1;  i >= 0 && stay == 0; i--) {
-		/* let i count from the back and count down. the first 
-		a[i] < a[i + 1] is the biggest a[j]. Once found, stay == 1
-		will jump out the for loop. we initialize j == -1, since
-		it is impossible for j == -1, if no j found, the value remains
-		and it's time to jump out the while loop */
-			if (a[i] < a[i + 1]) { 	//a[j] < a[j + 1]
-				j = i;				// store in j
-				stay = 1;			// jump out the loop
-				counter++;			// one more permutation found
-			}
-		}
-		if (j == -1)		// no more a[j] < a[j + 1] found
-			stay = 3;	 	// jump out the while loop
-		for (k = N - 1; k >= 0 && stay == 1; k--) {
-		/* count k from the back, the first one to find is the
-		biggest a[k] > a[j], then swap */ 
-			if (a[k] > a[j]) {
-				temp = a[k];
-				a[k] = a[j];
-				a[j] = temp;	// swap arrays
-				stay = 2;		// jump to the next procedure
-			}
-		}
-		if (stay != 3)			// if no j found then don't print
-			printf("permutation #%d:", counter);
-		for (i = j + 1, m = N - 1; i < m && stay == 2; i++, m--) {
-		/* we want to swap from a[j + 1] to a[N - 1], let the
-		a[j + 1] and a[N - 1] swap first, then a[j + 2] and a[N - 2}
-		.... and so on, until the two arrays are the same 
-		or neighboring */
-			temp = a[i];		// swap
-			a[i] = a[m];
-			a[m] = temp;
-		}
-		for (i = 0; i < N && stay!= 3; i++) { // print out permutaion	
-			printf("%2d", a[i]);
-		}
-		if (stay != 3) {		// if no j found don't print
-			printf("\n");
-		}
-	}
-	// last line
-	printf("  Total number of permutations is %d\n", counter);
-		return 0;
+    int i, j, k, a[N], temp, counter = 0;
+    int last = 0, found = 0;   // flag for last pemutation and found permutation
+    /* i, j, k are loop index for the permutation,
+       a[N] will store the array, 
+       temp is for swapping
+       counter is for recording the number of permutation
+    */
+    printf("permutation #%d: ", ++counter);	// first line
+    for (i = 0; i < N; i++) {
+        a[i] = i + 1;
+        printf("%d ", a[i]);        // print out first permutation
+    }
+    printf("\n");
+    while (!last) {                 // if not last permutation
+        found = 0;
+        // starting from the back to find largest j
+        for (j = N - 2; j >= 0 && !found; j--) {
+            if (a[j] < a[j + 1]) {      // a[j] < a[j + 1]
+            // staring from the back to find the largest k
+                for (k = N - 1; k >= 0 && !found; k--) {
+                    if (a[j] < a[k]) {  // a[j] < a[k]
+                        found = 1;      // set flag to found
+                        temp = a[j];    // swap a[j] with a[k]
+                        a[j] = a[k];
+                        a[k] = temp;
+                        // reversing the series
+                        for (i = j + 1; i <= (N + j) / 2; i++) {
+                            temp = a[i];
+                            a[i] = a[N - i + j];
+                            a[N - i + j] = temp;
+                        }
+                    }
+                }   // print out the permutation
+                printf("permutation #%d: ", ++counter);    
+                for (i = 0; i < N; i++) {
+                    printf("%d ", a[i]);
+                }
+                printf("\n");
+            }
+        }
+        if (!found)         // if no j found, it is the last permutation
+            last = 1;       // set last flag
+    }
+    printf("  Total number of permutations is %d", counter);
+    return 0;
 }
